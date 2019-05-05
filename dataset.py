64d7d9473b105e9625beae07408745d01cda98d4
Gaurav <Kudva>
2019-05-05 17:13:45 +0200

    Semcor Final

--- dataset.py
+++ dataset.py
@@ -2,33 +2,35 @@ class DatasetPreProcessor:
   
   def __init__(self, corpus = 'semcor', first_time = False):
     
-    self.corpus = nltk.corpus.semcor if corpus == 'semcor' else nltk.corpus.masc
+    self.corpus_name = corpus
+    
+    self.corpus = nltk.corpus.semcor if self.corpus_name == 'semcor' else nltk.corpus.masc
     
     self.wordnet = nltk.corpus.wordnet
     
     self.num_docs = len(self.corpus.fileids())
     
+    self.get_vocab()
+    
     if first_time:
       self.document = self.preprocess_corpus()
-      with open("document_" + corpus + ".pickle", 'wb') as f:
+      with open("document_" + corpus + ".pckl", 'wb') as f:
         pickle.dump(self.document, f)
     else:
-      with open("document_" + corpus + ".pickle", 'rb') as f:
+      with open("document_" + corpus + ".pckl", 'rb') as f:
         self.document = pickle.load(f)
+        
+    self.get_embeddings(self.w2i, first_time = first_time)
   
   def get_index_2_sentence(self, doc_id):
     
     return {i : s for i, s in enumerate(self.corpus.sents(doc_id))}
   
-  def get_vocab(self, doc_ids):
+  def get_vocab(self):
     
-    vocab = set(self.corpus.words(doc_ids))
+    self.vocab = set(self.corpus.words())
     
-    w2i = {w : i for i, w in enumerate(list(vocab))}
-    
-    i2w = {i : w for i, w in enumerate(list(vocab))}
-    
-    return vocab, w2i, i2w
+    self.w2i = {w : i for i, w in enumerate(list(self.vocab))}
   
   def get_document(self, doc_id):
     
@@ -149,40 +151,48 @@ class DatasetPreProcessor:
             
     return context
   
-  def get_embeddings(self, word_to_index, embed_dim = 300, embed_type = 'glove'):
+  def get_embeddings(self, word_to_index, embed_dim = 300, embed_type = 'glove', first_time = False):
 
-    if embed_type == 'glove':
-      with open('glove_model.pckl', 'rb') as f:
-        embed_model = pickle.load(f)
+    if first_time:
+      
+      if embed_type == 'glove':
+        embed_model = GloVe()
+      
+      self.embedding = torch.nn.Embedding(len(word_to_index) + 4, embed_dim)
+
+      weight_matrix = np.zeros((len(word_to_index) + 4, embed_dim))
+      
+      average_vector = np.zeros((1, embed_dim))
+
+      weight_matrix[2:4, :] = np.random.randn(1, embed_dim)
+
+      for word in word_to_index:
+        
+        average_vector += average_vector + embed_model[word].numpy()
+        
+        weight_matrix[word_to_index[word] + 4, :] = embed_model[word].numpy()
+
+      weight_matrix[1, :] = average_vector / len(word_to_index)
+      
+      self.embedding.weight.data.copy_(torch.from_numpy(weight_matrix))
+      
+      with open(self.corpus_name + "_" + embed_type + "_vectors.pckl", 'wb') as f:
+        pickle.dump(self.embedding, f)
+    
     else:
-      pass
-
-    embedding = torch.nn.Embedding(len(word_to_index) + 4, embed_dim)
-
-    weight_matrix = np.zeros((len(word_to_index) + 4, embed_dim))
-    
-    weight_matrix[1:4, :] = np.random.randn(1, embed_dim)
-    
-    for word in word_to_index:
-      weight_matrix[word_to_index[word] + 4, :] = embed_model[word].numpy()
-
-    embedding.weight.data.copy_(torch.from_numpy(weight_matrix))
-  
-    return embedding
+      
+      with open(self.corpus_name + "_" + embed_type + "_vectors.pckl", 'rb') as f:
+          self.embedding = pickle.load(f)
     
   def get_train_validation_split(self, split):
     
-    self.doc_ids, self.vocab, self.i2w, self.w2i = {}, {}, {}, {}
+    self.doc_ids = {}
     
     split_index = math.floor(self.num_docs * split)
     
     self.doc_ids['train'] = self.corpus.fileids()[0 : split_index]
     
     self.doc_ids['dev'] = self.corpus.fileids()[split_index : ]
-    
-    self.vocab['train'], self.w2i['train'], self.i2w['train'] = self.get_vocab(self.doc_ids['train'])
-    
-    self.vocab['dev'], self.w2i['dev'], self.i2w['dev'] = self.get_vocab(self.doc_ids['dev'])
     
   def preprocess_corpus(self):
     
